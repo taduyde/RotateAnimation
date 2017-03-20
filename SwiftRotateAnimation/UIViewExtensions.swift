7d87397b58899ae0de2038986e9e06e34e0c62e9
Robert Ta <taduydeit@gmail.com>
2017-03-20 11:26:21 +0700

    Update UIViewExtensions.swift

--- SwiftRotateAnimation/UIViewExtensions.swift
+++ SwiftRotateAnimation/UIViewExtensions.swift
@@ -12,4 +12,14 @@ extension UIView {
 		}
 		self.layer.add(rotateAnimation, forKey: nil)
 	}
+	func fadeIn(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+		UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+			self.alpha = 1.0
+			}, completion: completion)	}
+	
+	func fadeOut(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+		UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+			self.alpha = 0.0
+			}, completion: completion)
+	}
 }
